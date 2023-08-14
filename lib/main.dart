4d9fc1220ee0a672e7f4718ae79dbf469b23f573
alfism <alfialfarisi@gmail.com>
2023-08-14 07:53:53 +0700

    change color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'package:first_app/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 11, 145, 255),
+);
+
 void main() {
   runApp(
     MaterialApp(
       title: 'Flutter Tutorial',
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: const Expenses(),
     ),
   );
